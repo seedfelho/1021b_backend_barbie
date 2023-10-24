ffd54113fd5a46ea06e0e827a733d4d52ddb09df
guilhermeterenciani <guilhermeterenciani@gmail.com>
2023-10-24 10:51:52 -0400

    Correção await async

--- test/index.test.ts
+++ test/index.test.ts
@@ -6,14 +6,14 @@ describe('Primeiro teste da aplicação',()=>{
     })
 })
 describe('Cadastro de filmes',()=>{
-    test('Espero que o filme seja cadastrado',()=>{
+    test('Espero que o filme seja cadastrado',async ()=>{
         const filme = {
             titulo:'Vingadores',
             descricao:'Filme dos herois da marvel',
             imagem:"https://live.staticflickr.com/7270/6976087418_59719341f5_b.jpg"
         }
         //Passar isso para minha API
-        const resposta = axios.post('http://localhost:3000/filmes',filme)
+        const resposta = await axios.post('http://localhost:3000/filmes',filme)
         //Esperar que minha API tenha cadastrado o filme
         expect(resposta.status).toBe(201)
         expect(resposta.data).toEqual(filme)
