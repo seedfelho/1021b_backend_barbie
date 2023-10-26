92a90cf25a77280eb9d8dbace6093381fc6ff9e9
guilhermeterenciani <guilhermeterenciani@gmail.com>
2023-10-26 09:42:16 -0400

    exercicio2) Como salvar o filme no vetor filmesCadastros?

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,25 @@ import express from 'express'
 const app = express()
 app.use(express.json())
 //Tenho que ter uma rota post para cadastrar um filme
+
+//Salvar em algum lugar o filme que foi cadastrado.
+type Filme = {
+    id:number,
+    titulo:string,
+    descricao:string,
+    imagem:string
+}
+let filmesCadastros:Filme = []
 app.post('/filmes',(req,res)=>{
+    const {id,titulo,descricao,imagem} = req.body
     const filme = {
-        id:1,
-        titulo:'Vingadores',
-        descricao:'Filme dos herois da marvel',
-        imagem:"https://live.staticflickr.com/7270/6976087418_59719341f5_b.jpg"
+        id,
+        titulo,
+        descricao,
+        imagem
     }
+    //Como eu salvo o filme que foi cadastrado no meu vetor de filmes (Banco de dados)
+    //???
     res.status(201).send(filme)
 })
 app.get('/filmes',(req,res)=>{
--- test/index.test.ts
+++ test/index.test.ts
@@ -28,7 +28,7 @@ describe('Cadastro de filmes',()=>{
         expect(resposta.status).toBe(201)
         expect(await resposta.json()).toEqual(filme)
     })
-    test('Espero que o filme cadastrado seja listado',async ()=>{
+    test('Espero que cadastre um segundo filme',async ()=>{
         const filme = {
             id:2,
             titulo:'Vingadores',
@@ -45,14 +45,36 @@ describe('Cadastro de filmes',()=>{
                         },
                         body:JSON.stringify(filme)
                     }
-        )
+                    )
         //Esperar que minha API tenha cadastrado o filme
         expect(resposta.status).toBe(201)
         expect(await resposta.json()).toEqual(filme)
-
-        //Listar os filmes cadastrados
-        const respostaGet = await fetch('http://localhost:3000/filmes/2')
-        expect(respostaGet.status).toBe(200)
-        expect(await respostaGet.json()).toEqual([filme])
     })
+    // test('Espero que o filme cadastrado seja listado',async ()=>{
+    //     const filme = {
+    //         id:2,
+    //         titulo:'Vingadores',
+    //         descricao:'Filme dos herois da marvel',
+    //         imagem:"https://live.staticflickr.com/7270/6976087418_59719341f5_b.jpg"
+    //     }
+    //     //Passar isso para minha API
+    //     const resposta = await fetch(
+    //                 'http://localhost:3000/filmes',
+    //                 {
+    //                     method:'POST',
+    //                     headers:{
+    //                         'Content-Type':'application/json'
+    //                     },
+    //                     body:JSON.stringify(filme)
+    //                 }
+    //     )
+    //     //Esperar que minha API tenha cadastrado o filme
+    //     expect(resposta.status).toBe(201)
+    //     expect(await resposta.json()).toEqual(filme)
+
+    //     //Listar os filmes cadastrados
+    //     const respostaGet = await fetch('http://localhost:3000/filmes/2')
+    //     expect(respostaGet.status).toBe(200)
+    //     expect(await respostaGet.json()).toEqual([filme])
+    // })
 })
