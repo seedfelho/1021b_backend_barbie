bb1e27e99fefb619486ad5633b825d9324852d93
guilhermeterenciani <guilhermeterenciani@gmail.com>
2023-11-09 09:22:14 -0400

    Criando o schema do banco_mongo

--- src/infra/banco/banco-mongodb.ts
+++ src/infra/banco/banco-mongodb.ts
@@ -1,7 +1,23 @@
+import mongoose from "mongoose";
 import FilmeRepositorioInterface from "../../aplicacao/filme-repositorio-interface";
-
+require("dotenv").config();
 export class BancoMongoDB implements  FilmeRepositorioInterface {
-  // implementação
+  public filmeModelo: any;
+  constructor() {
+    try{
+      mongoose.connect(process.env.MONGODB_URL || "")
+    }catch(error){
+      console.log(error)
+    }
+    this.filmeModelo = mongoose.model("Filme", 
+        new mongoose.Schema({
+            _id: Number,
+            titulo: String,
+            descricao: String,
+            imagem: String
+        })
+    )
+  }
   async salvar(filme: Filme): Promise<Filme> {
     return new Promise((resolve) => {
       resolve(filme);
