d0d8f9befb6293630473c9505e6fb4c063933f99
guilhermeterenciani <guilhermeterenciani@gmail.com>
2023-11-14 07:55:10 -0400

    Criando o beforeEach

--- src/aplicacao/lista-filme.use-case.test.ts
+++ src/aplicacao/lista-filme.use-case.test.ts
@@ -1,8 +1,26 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, test,beforeEach } from "vitest";
 import ListaFilme from "./lista-filme.use-case";
 import BancoEmMemoria from "../infra/banco/banco-em-memoria";
+import { b } from "vitest/dist/reporters-5f784f42";
 describe("ListaFilme", () => {
     const bancoEmMemoria = new BancoEmMemoria();
+
+    beforeEach(async () => {
+        bancoEmMemoria.filmes = [{
+            id: 1,
+            titulo: "O Poderoso Chefão",
+            descricao: "Filme de máfia",
+            imagem: "fotofilme.jpg"
+        },
+        {
+            id: 2,
+            titulo: "O Poderoso Chefão 2",
+            descricao: "Filme de máfia",
+            imagem: "fotofilme.jpg"
+        
+        }]
+    });
+
     test("deve retornar uma lista de filmes", async () => {
         const listaFilme = new ListaFilme(bancoEmMemoria);
         const filmes = await listaFilme.executar();
